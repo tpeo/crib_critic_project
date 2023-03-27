a698e628e95d83d9bbfe8b9d9946e5434368ed6f
Aaryan Sagar <aaryansagar22@gmail.com>
2023-03-27 17:44:42 -0500

    Interim changes for webscraper

--- test-webscraper/webscraper.mjs
+++ test-webscraper/webscraper.mjs
@@ -1,73 +1,157 @@
 import fetch from 'node-fetch';
 import cheerio from 'cheerio';
 import puppeteer from 'puppeteer';
+import {db} from './firebase.mjs';
 
-async function getRating(names) {
-  const apartment = names.trim().toLowerCase().replace(/\s+/g, '+');
-  try {
-    //Fetches the webpage
-    const response = await fetch(
-      "https://www.google.com/search?q=" +
-        apartment +
-        "&sourceid=chrome&ie=UTF-8"
-    );
-    //Converts the response to html
-    const html = await response.text();
-    //Loads the html into a Cheerio object
-    const $ = cheerio.load(html);
-    //Selects a div that contains a span with an aria-hidden attribute that equals true 
-    //(selects the parent div)
-    const divWithSpans = $('div').find('span[aria-hidden="true"]').parent();
-    //Isolates the rating value within the first span of the text
-    var stars = Number.parseFloat(divWithSpans.find('span').eq(0).text());
-    //Isolates the number of ratings within the second span of the text
-    var numRatings = Number.parseInt(divWithSpans.find('span').eq(2).text().replace(/[^\d]/g, ''))
-    //Repeats the process if an error is encountered until a specific answer is found (occasionally gets parsed incorrectly)
-    while (isNaN(stars) || isNaN(numRatings)) {
-      [stars, numRatings] = getRating(names);
-    }
-    return [stars, numRatings]
-  } catch (error) {
-    console.error(error);
-  }
-}
+// async function getApartmentRatings(apartmentName) {
+//   const apartment = apartmentName.trim().toLowerCase().replace(/\s+/g, '+');
+//   const url = "https://www.google.com/search?q=" + apartment + "&sourceid=chrome&ie=UTF-8";
+  
+//   try {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+    
+//     await page.goto(url, { waitUntil: 'networkidle2' });
+//     await page.waitForSelector('div.g');
+    
+//     const ratingText = await page.$eval('div[aria-label^="Rated"]', el => el.textContent);
+//     const reviewText = await page.$eval('span[jsname="fk8dgd"]', el => el.textContent);
+    
+//     const rating = parseFloat(ratingText.match(/[\d|.]+/));
+//     const reviewCount = parseInt(reviewText.replace(/[^\d]/g, ''));
+    
+//     await browser.close();
+    
+//     return [rating, reviewCount];
+//   } catch (error) {
+//     console.error(error);
+//     return null;
+//   }
+// }
+
+
+
+// async function getApartments() {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto('https://www.google.com/search?sa=X&tbs=lf:1,lf_ui:2&tbm=lcl&q=west+campus+apartments&rflfq=1&num=10&ved=2ahUKEwiSwNPM9fz9AhV1nGoFHdR6A2QQjGp6BAhmEAI&biw=1440&bih=900&dpr=2#rlfi=hd:;si:;mv:%5B%5B30.294223,-97.7412775%5D,%5B30.2835274,-97.7527688%5D%5D;start:0');
+  
+//   const results = [];
+  
+//   for (let i = 0; i < 4; i++) {
+//     await page.waitForSelector('.dbg0pd');
+    
+//     const apartmentNames = await page.$$eval('.dbg0pd', apartments => {
+//       return apartments.map(apartment => apartment.textContent.trim());
+//     });
+    
+//     results.push(...apartmentNames);
+    
+//     const nextButton = await page.$('#pnnext');
+//     console.log(nextButton);
+    
+//     if (nextButton) {
+//       await nextButton.click();
+//       await page.waitForNavigation();
+//     } else {
+//       break;
+//     }
+//   }
+  
+//   await browser.close();
+//   results.forEach(e => addDocument(e));
+//   // results.forEach(e => addDocument(e))
+// }
+
+// getApartments();
+
+// function addDocument(name) {
+//   const collectionRef = db.collection('apartments');
+//   const data = {};
+//   collectionRef
+//     .doc(name)
+//     .set(data)
+//     .then(() => {
+//       console.log(`Document with ID ${name} written successfully`);
+//     })
+//     .catch((error) => {
+//       console.error("Error adding document:", error);
+//     });
+// }
+
+// function addRatings() {
+//   const collectionRef = db.collection('apartments');
+//   collectionRef.get().then(querySnapshot => {
+//     querySnapshot.forEach(async doc => {
+//         var values = await getApartmentRatings(doc.id);
+//         console.log(values);
+//         // doc.ref.update(values)
+//     });
+//   })
+// }
+
 
 
 async function getApartments() {
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    await page.goto(
-      "https://www.google.com/search?tbs=lf:1,lf_ui:2&tbm=lcl&q=west+campus+apartments&rflfq=1&num=10&sa=X&ved=2ahUKEwiB0riRwPr9AhVVnWoFHUe9DBgQjGp6BAhoEAI&biw=1440&bih=900&dpr=2#rlfi=hd:;si:;mv:%5B%5B30.295229699999997,-97.7413794%5D,%5B30.2827637,-97.7509692%5D%5D;tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:2"
-    );
-
-    // Wait for the page to load
-    await page.waitForSelector(".dbg0pd");
-
-    // Extract the apartment names
-    const apartmentNames = await page.$$eval(".dbg0pd", (apartments) =>
-      apartments.map((apartment) => apartment.textContent.trim())
-    );
-    console.log(apartmentNames);
-
-    await browser.close();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.google.com/search?sa=X&tbs=lf:1,lf_ui:2&tbm=lcl&q=west+campus+apartments&rflfq=1&num=10&ved=2ahUKEwiSwNPM9fz9AhV1nGoFHdR6A2QQjGp6BAhmEAI&biw=1440&bih=900&dpr=2#rlfi=hd:;si:;mv:%5B%5B30.294223,-97.7412775%5D,%5B30.2835274,-97.7527688%5D%5D;start:0');
+  const results = [];
+  
+  for (let i = 0; i < 4; i++) {
+    console.log(1);
+    await page.waitForSelector('dbg0pd');
+    console.log(1);
+    const apartments = await page.$$('.cXedhc');
+    
+    for (const apartment of apartments) {
+      const name = await apartment.$('span.OSrXXb', title => title.textContent.trim());
+      const ratingEl = await apartment.$('span[aria-hidden="true"]');
+      const rating = ratingEl ? await ratingEl.evaluate(el => Number.parseFloat(el.getAttribute("aria-label"))) : null;
+      const numRatingsEl = await apartment.$('span:nth-child(4)');
+      const numRatings = numRatingsEl ? await numRatingsEl.evaluate(el => Number.parseInt(el.textContent.replace(/[^\d]/g, ''))) : null;
+      results.push({name, rating, numRatings});
+    }
+    
+    const nextButton = await page.$('#pnnext');
+    
+    if (nextButton) {
+      await nextButton.click();
+      await page.waitForNavigation();
+    } else {
+      break;
+    }
   }
-  catch (error) {
-    console.log(error);
-  }
+
+  await browser.close();
+  // results.forEach(e => addDocument(e));
+  console.log(results);
 }
 
-//Test function (checked and works)
-// async function main() {
-//     const larkRating = await getRating("Lark Austin");
-//     console.log("Lark Austin :::   Stars: ", larkRating[0], ", Number of Ratings: ", larkRating[1]);
 
-//     const villasRating = await getRating("Villas on Rio");
-//     console.log("Villas on Rio ::: Stars: ", villasRating[0], ", Number of Ratings: ", villasRating[1]);
+// async function addRatings() {
+//   try {
+//     const apartmentsRef = db.collection('apartments');
+//     const querySnapshot = await apartmentsRef.get();
 
-//     const ionRating = await getRating("Ion Austin");
-//     console.log("Ion Austin :::    Stars: ", ionRating[0], ", Number of Ratings: ", ionRating[1]);
+//     const promises = [];
+
+//     querySnapshot.forEach(async (doc) => {
+//       // const promise = getApartmentRatings(doc.id).then((values) => {
+//       //   if (values) {
+//       //     return doc.ref.update(values);
+//       //   }
+//       // console.log()
+//       // });
+//       console.log(doc.id)
+//       console.log(await getApartmentRatings(doc.id));
+//       // promises.push(promise);
+//     });
+
+//     // await Promise.all(promises);
+//   } catch (error) {
+//     console.error(error);
+//   }
 // }
 
 getApartments();
