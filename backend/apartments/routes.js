ff0606a159f8cc29c35ee8a1f3f3e00064384d5c
Aaryan Sagar <aaryansagar22@gmail.com>
2023-04-05 19:38:05 -0500

    APIs

--- backend/apartments/routes.js
+++ backend/apartments/routes.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const middleware = require("../middleware/middle_functions");
-const firebase = require("../firebase");
+const {db} = require("../firebase");
 const auth = express.Router();
 require("dotenv").config();
 auth.use(cors());
 auth.use(express.json());
 
-auth.get("/", middleware.auth(req, res, next), (req, res) => {
+auth.get("/apartment_list", middleware.auth(req, res, next), async (req, res) => {
+    try {
+        const groups = await db.collection('groups').get();
+    }
+    catch(error) {
+        res.status(400).json(error);
+    }
 });
 
-auth.post("/", middleware.auth(req, res, next), (req, res) => {
+auth.post("/amenities", middleware.auth(req, res, next), (req, res) => {
+    try {
 
+    }
+    catch(error) {
+
+    }
 });
+
+module.exports = {auth};
--- backend/firebase.js
+++ backend/firebase.js
@@ -8,4 +8,4 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-module.exports = {admin, db}
+module.exports = {db}
