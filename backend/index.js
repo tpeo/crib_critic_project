c0ecda5946a100171c58092ab43a9fb729e6149f
Aaryan Sagar <aaryansagar22@gmail.com>
2023-04-09 18:01:31 -0500

    Renamed directories

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,8 @@ const app = express();
 
 app.use(express.json());
 require("dotenv").config();
-const {apartmentRouter} = require('./apartments/routes.js');
+const {apartmentRouter} = require('./routes/apartments.js');
 
 app.use("/apartments", apartmentRouter);
-
 
 app.listen(process.env.PORT, () => console.log("Listening on port ", process.env.PORT));
