2862d0c12604c6818f487626ebec306c53fd03f2
Aaryan Sagar <aaryansagar22@gmail.com>
2023-04-10 14:58:04 -0500

    Added Card listings

--- backend/routes/apartments.js
+++ backend/routes/apartments.js
@@ -32,6 +32,26 @@ apartmentRouter.get("/apartment_features/:name", async (req, res) => {
       res.status(400).json(error);
     }
   });
+
+apartmentRouter.get("/card_list", async (req, res) => {
+  try {
+    const apartments = await db.collection('apartments').get();
+    const apartmentNames = [];
+    apartments.forEach((doc) => {
+      console.log(doc)
+      console.log(doc.data())
+      apartmentNames.push({
+        name: doc.id,
+        address: doc.data().address,
+        image: doc.data().picture,
+      });
+    });
+    res.status(200).json({apartmentNames: apartmentNames});
+}
+catch(error) {
+  res.status(400).json(error);
+}
+})
   
 
 module.exports = {apartmentRouter};
