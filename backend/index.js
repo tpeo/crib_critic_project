0ff5ac94944a22ce5512a01a4b668c98c7b02b65
Aaryan Sagar <aaryansagar22@gmail.com>
2023-04-10 13:27:37 -0500

    Added User endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-
 app.use(express.json());
 require("dotenv").config();
 const {apartmentRouter} = require('./routes/apartments.js');
+const {userRouter} = require('./routes/users.js');
 
 app.use("/apartments", apartmentRouter);
+
+app.use('/users', userRouter);
 
 app.listen(process.env.PORT, () => console.log("Listening on port ", process.env.PORT));
